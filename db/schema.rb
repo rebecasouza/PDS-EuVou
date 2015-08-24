b6e8068c17f4f66a761f635f446c8b686304c5ab
LuanGB <luan.goncbs@gmail.com>
2015-08-24 11:30:31 -0300

    Modificação de banco de dados de produção para postgresql, alteração nas configurações de banco de dados

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150816115904) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "titulo"
     t.string   "icone"
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 20150816115904) do
     t.integer "event_id",    null: false
   end
 
-  add_index "categories_events", ["event_id", "category_id"], name: "index_categories_events_on_event_id_and_category_id"
+  add_index "categories_events", ["event_id", "category_id"], name: "index_categories_events_on_event_id_and_category_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.text     "conteudo"
@@ -36,8 +39,8 @@ ActiveRecord::Schema.define(version: 20150816115904) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["event_id"], name: "index_comments_on_event_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+  add_index "comments", ["event_id"], name: "index_comments_on_event_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "events", force: :cascade do |t|
     t.string   "titulo"
@@ -54,14 +57,14 @@ ActiveRecord::Schema.define(version: 20150816115904) do
     t.datetime "avatar_updated_at"
   end
 
-  add_index "events", ["creator_id"], name: "index_events_on_creator_id"
+  add_index "events", ["creator_id"], name: "index_events_on_creator_id", using: :btree
 
   create_table "events_users", id: false, force: :cascade do |t|
     t.integer "event_id", null: false
     t.integer "user_id",  null: false
   end
 
-  add_index "events_users", ["user_id", "event_id"], name: "index_events_users_on_user_id_and_event_id"
+  add_index "events_users", ["user_id", "event_id"], name: "index_events_users_on_user_id_and_event_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "nome",                                   null: false
@@ -93,7 +96,7 @@ ActiveRecord::Schema.define(version: 20150816115904) do
     t.datetime "avatar_updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
