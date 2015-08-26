ab43d03a250262b9cf740688f09e9557970db67d
Renato Soares <renatosoarespro@hotmail.com>
2015-08-26 07:53:56 -0300

    mesclando arquivos dos repositórios

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :events
   has_many :events_created, foreign_key: "creator_id", class_name: 'Event'
   has_many :comments
-  has_attached_file :avatar, :styles => { :medium => "100x100>", :thumb => "64x64>" }, :default_url => "missing.png"
 
+  has_attached_file :avatar, :styles => { :medium => "100x100>", :thumb => "64x64>" }, :default_url => "missing.png" 
 
   validates_presence_of :nome
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
@@ -17,15 +17,17 @@ class User < ActiveRecord::Base
 
   scope :admins, -> { where(admin: true) }
 
-  def my_level
+  def my_level      # gambiarras, depois tu melhora Marcelo ;)
     if (0..100).include?(self.pontuacao)
-      'Saidinho'
+      ['Saidinho', 'fa fa-rocket']            # <i class="fa fa-hand-peace-o"></i>
     elsif (101..300).include?(self.pontuacao)
-      'Baladeiro'
+      ['Baladeiro', 'fa fa-bullhorn']           # <i class="fa fa-bullhorn"></i>
+      
     elsif (301..900).include?(self.pontuacao)
-      'Baladeiro Supremo'
+      ['Baladeiro Supremo', 'fa fa-rocket']   # <i class="fa fa-rocket"></i>
+       
     else
-      'Rei do Camarote'
+      ['Rei do Camarote', 'fa fa-diamond']     # <i class="fa fa-diamond"></i>
     end
   end
 
