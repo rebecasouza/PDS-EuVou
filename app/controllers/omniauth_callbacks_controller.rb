c701a0ca0d6863420fa00f53b9d110997461de5b
rebecasouza <rebecasouza1985@gmail.com>
2015-11-23 07:39:59 -0300

    more changes in the callback-method

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     user = User.from_omniauth(request.env['omniauth.auth'])
     if user.persisted?
-      sign_in_and_redirect user
+      sign_in_and_redirect root_url
     else
       redirect_to user_session_path
     end
