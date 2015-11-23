649a30d48e338db47812823dc7c81334d5ea6060
rebecasouza <rebecasouza1985@gmail.com>
2015-11-23 02:42:18 -0300

    changing the callback redirect uri

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,9 +4,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 #para fazer login por mais de uma rede social
   def facebook
     auth = env['omniauth.auth']
-    @user = User.from_omniauth(request.auth, current_user)
+    @user = User.from_omniauth(request.auth)
     if @user.persisted?
-      sign_in_and_redirect @user, notice: 'Login efetuado com sucesso'
+      sign_in_and_redirect root_path
     else
       redirect_to user_session_path
     end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'events#index'
-
+  
+  get 'auth/:provider/callback' => 'omniauth_callbacks#facebook'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
